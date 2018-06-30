9080bcbb82f8c5778f4ddaf5ba2070dfbf094b57
alpine-fury <sean151@gmail.com>
2018-06-30 11:03:54 -0700

    Initial working page for inventory view page completed.

--- app/models/awms_auin_attribute.rb
+++ app/models/awms_auin_attribute.rb
@@ -1,5 +1,5 @@
-class AwmsUinAttribute < ActiveRecord::Base
-  self.table_name = 'uin_attributes'
+class AwmsAuinAttribute < ActiveRecord::Base
+  self.table_name = 'auin_attributes'
   self.primary_keys = :uin
   def self.column_headers
     self.column_names.map {|e| e.to_sym}
--- inventory/app/controllers/inventory/views_controller.rb
+++ inventory/app/controllers/inventory/views_controller.rb
@@ -5,7 +5,10 @@ module Inventory
     end
 
     def get_inv_views
-      @inv_views_results = AwmsUinAttribute.where(uin: params[:auin])
+      @inv_views_attributes = AwmsAuinAttribute.where(auin: params[:auin])
+      @inv_views_attributes = @inv_views_attributes.first if !@inv_views_attributes.blank?
+      puts @inv_views_attributes.inspect
+      @inv_views_locations = AwmsInvItem.where(auin: params[:auin])
     end
 
   end
